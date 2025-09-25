860a8fe900581b370f721112135df872c808178d
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2025-09-25 13:39:29 -0300

    Pronto

--- js.js
+++ js.js
@@ -1,14 +1,20 @@
 const form = document.getElementById("form");
-  const tabela = document.querySelector("#tabela tbody");
+  const tabela = document.querySelector("#tabela .corpo-tabela");
   const dataInput = document.getElementById("data");
   const horarioInput = document.getElementById("horario");
   const cargaInput = document.getElementById("cargaHoraria");
-  const totalGeralEl = document.getElementById("totalExtra");
-
-  let totalExtras = 0;
+  
   let jornadaDiarias = JSON.parse(localStorage.getItem("jornadaDiarias")) || [];
   let cargaHorarias = JSON.parse(localStorage.getItem("cargaHorarias")) || [];
 
+  let totalTrabalhadas = 0;
+  let totalPositiva = 0;
+  let totalNegativa = 0;
+
+  const totalHoraTrabalhadas = document.getElementById("totalTrabalhadas");
+  const totalHoraPositiva = document.getElementById("totalHoraPositiva");
+  const totalHoraNegativa = document.getElementById("totalHoraNegativa");
+  
   // Preenche a data de hoje automaticamente
   const hoje = new Date();
   dataInput.value = hoje.toISOString().split("T")[0];
@@ -67,7 +73,9 @@ const form = document.getElementById("form");
 
   function montarTabela() {
     tabela.innerHTML = "";
-    totalExtras = 0;
+    totalTrabalhadas = 0;
+    totalPositiva = 0;
+    totalNegativa = 0;
 
     jornadaDiarias.forEach(r => {
       montarLinha(r.data, r.horarios);
@@ -82,50 +90,40 @@ const form = document.getElementById("form");
     const diaSemana = dataCerta.toLocaleDateString("pt-BR", { weekday: "long" });
     const dataFormatada = dataCerta.toLocaleDateString("pt-BR");
 
-    const linha = document.createElement("tr");
+    const linha = document.createElement("div");
+    linha.classList.add("linha-tabela");
     linha.dataset.data = data;
     linha.innerHTML = `
-      <td>${diaSemana}</td>
-      <td>${dataFormatada}</td>
-      <td class="entrada">${horarios[0] || ""}</td>
-      <td class="saida-almoco">${horarios[1] || ""}</td>
-      <td class="retorno">${horarios[2] || ""}</td>
-      <td class="saida-final">${horarios[3] || ""}</td>
-      <td class="horas-trabalhadas"></td>
-      <td class="hora-extra"></td>
+        <p>${diaSemana}</p>
+        <p>${dataFormatada}</p>
+        <p class="entrada">${horarios[0] || ""}</p>
+        <p class="saida-almoco">${horarios[1] || ""}</p>
+        <p class="retorno">${horarios[2] || ""}</p>
+        <p class="saida-final">${horarios[3] || ""}</p>
+        <p class="horas-trabalhadas"></p>
+        <p class="hora-extra"></p>
     `;
     tabela.appendChild(linha);
 
     calcularHoras(linha, horarios);
   }
 
-  function calcularHoras(linha, horarios) {
-    const entrada = horarios[0];
-    const saidaAlmoco = horarios[1];
-    const retorno = horarios[2];
-    const saidaFinal = horarios[3];
+function calcularHoras(linha, horarios) {
+    const [entrada, saidaAlmoco, retorno, saidaFinal] = horarios;
 
     if (entrada && retorno && saidaFinal) {
       let totalMin = 0;
 
-      if (entrada && saidaAlmoco) {
-        totalMin += diffMinutos(entrada, saidaAlmoco);
-      }
-
-      if (retorno && saidaFinal) {
-        totalMin += diffMinutos(retorno, saidaFinal);
-      }
+      if (entrada && saidaAlmoco) totalMin += diffMinutos(entrada, saidaAlmoco);
+      if (retorno && saidaFinal) totalMin += diffMinutos(retorno, saidaFinal);
 
       const horas = Math.floor(totalMin / 60);
       const minutos = totalMin % 60;
       linha.querySelector(".horas-trabalhadas").textContent = `${horas}h ${minutos}m`;
 
       const ultimaCargaSalva = localStorage.getItem("ultimaCargaSalva");
-      if (cargaInput.value || ultimaCargaSalva) {
-        const carga = (ultimaCargaSalva || cargaInput.value)
-          ? parseInt(ultimaCargaSalva || cargaInput.value) * 60
-          : 0;
-
+      if (cargaInput?.value || ultimaCargaSalva) {
+        const carga = parseInt(ultimaCargaSalva || cargaInput.value) * 60
         const extraMin = totalMin - carga;
         let extraTexto = "0h";
 
@@ -135,14 +133,19 @@ const form = document.getElementById("form");
           extraTexto = `${extraMin < 0 ? "-" : "+"}${h}h ${m}m`;
 
           if (extraMin > 0) {
-            totalExtras += extraMin; // só soma positivos ao total
+            totalPositiva += extraMin; // só soma positivos ao total
+          }
+            totalTrabalhadas += totalMin;
+
+          if (extraMin < 0) {
+            totalNegativa -= extraMin; // só soma negativos ao total
           }
         }
 
         linha.querySelector(".hora-extra").textContent = extraTexto;
       }
     }
-  }
+}
 
   function diffMinutos(hora1, hora2) {
     const [h1, m1] = hora1.split(":").map(Number);
@@ -151,9 +154,17 @@ const form = document.getElementById("form");
   }
 
   function atualizarTotal() {
-    const h = Math.floor(totalExtras / 60);
-    const m = totalExtras % 60;
-    totalGeralEl.textContent = `Total de Horas Extras: ${h}h ${m}m`;
+    const h = Math.floor(totalPositiva / 60);
+    const m = totalPositiva % 60;
+    totalHoraPositiva.textContent = `Total de Horas Positiva: ${h}h ${m}m`;
+
+    const j = Math.floor(totalNegativa / 60);
+    const n = totalNegativa % 60;
+    totalHoraNegativa.textContent = `Total de Horas Negativa: ${j}h ${n}m`;
+
+    const g = Math.floor(totalTrabalhadas / 60);
+    const t = totalTrabalhadas % 60;
+    totalHoraTrabalhadas.textContent = `Total de Horas Trabalhadas: ${g}h ${t}m`;
   }
 
   function salvarJornadaDiarias() {
